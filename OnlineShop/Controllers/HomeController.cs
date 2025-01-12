556212ba3718a408df4af3eb7e4a19d83d558226
Tom <tomek286@yahoo.com>
2025-01-12 06:47:48 +0100

    Banners dynamic

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Models;
+using OnlineShop.Models.Db;
 
 namespace OnlineShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly OnlineShopContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, OnlineShopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var banners = _context.Banners.ToList();
+            ViewData["banners"] = banners;
             return View();
         }
 
--- OnlineShop/Models/Db/OnlineShopContext.cs
+++ OnlineShop/Models/Db/OnlineShopContext.cs
@@ -17,6 +17,8 @@ public partial class OnlineShopContext : DbContext
     {
     }
 
+    public virtual DbSet<Banner> Banners { get; set; }
+ 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -24,7 +26,18 @@ public partial class OnlineShopContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-    
+        modelBuilder.Entity<Banner>(entity =>
+        {
+            entity.ToTable("Banner");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.ImageName).HasMaxLength(50);
+            entity.Property(e => e.Link).HasMaxLength(100);
+            entity.Property(e => e.Position).HasMaxLength(50);
+            entity.Property(e => e.SubTitle).HasMaxLength(1000);
+            entity.Property(e => e.Title).HasMaxLength(200);
+        });
+
 
         OnModelCreatingPartial(modelBuilder);
     }
