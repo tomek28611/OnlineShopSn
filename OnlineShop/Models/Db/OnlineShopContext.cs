120aa45a5b626a38bbaf90688c5ccbfbb67f36e8
Tom <tomek286@yahoo.com>
2025-01-12 09:25:07 +0100

    Products CRUD Admin

--- OnlineShop/Models/Db/OnlineShopContext.cs
+++ OnlineShop/Models/Db/OnlineShopContext.cs
@@ -18,7 +18,11 @@ public partial class OnlineShopContext : DbContext
     }
 
     public virtual DbSet<Banner> Banners { get; set; }
- 
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductGalery> ProductGaleries { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -36,6 +40,27 @@ public partial class OnlineShopContext : DbContext
             entity.Property(e => e.Position).HasMaxLength(50);
             entity.Property(e => e.SubTitle).HasMaxLength(1000);
             entity.Property(e => e.Title).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Discount).HasColumnType("money");
+            entity.Property(e => e.FullDesc).HasMaxLength(4000);
+            entity.Property(e => e.ImageName).HasMaxLength(50);
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.Tags).HasMaxLength(1000);
+            entity.Property(e => e.Title)
+                .HasMaxLength(200)
+                .HasColumnName("TItle");
+            entity.Property(e => e.VideoUrl).HasMaxLength(300);
+        });
+
+        modelBuilder.Entity<ProductGalery>(entity =>
+        {
+            entity.ToTable("ProductGalery");
+
+            entity.Property(e => e.ImageName).HasMaxLength(50);
         });
 
 
---
+++ OnlineShop/Models/Db/ProductGallery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShop.Models.Db;
+
+public partial class ProductGalery
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string? ImageName { get; set; }
+}
